3e4553246b9b2f62d789a1391951eda80602c5dd
Juan Contreras <JAContreras1@student.fullsail.edu>
2025-01-15 17:17:20 -0500

    Created Base Class for Items, to arrange in Inventory Class

---
+++ Assets/Scripts/Items/ItemBase.cs
@@ -0,0 +1,28 @@
+/*
+    Author: Juan Contreras
+    Date Created: 01/15/2025
+    Date Updated: 01/15/2025
+    Description: Abstract class to use when creating scriptable objects for items to be
+                 picked up in the game scene(s).
+ */
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public abstract class ItemBase : ScriptableObject
+{
+    [SerializeField] protected string itemName;
+    [SerializeField] protected Sprite icon;
+    [SerializeField] protected ItemType itemType;
+    [SerializeField] protected int maxStackSize = 1;
+    
+    public enum ItemType
+    {
+        Weapon,
+        Ammo,
+        Collectible,
+    }
+
+    public abstract void IntendedUse();
+}
