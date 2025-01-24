b2eefb9eb3d65d118a6f9a4a9113af0789cedaf2
Harry <harry.tanama@gmail.com>
2025-01-23 20:32:08 -0500

    creating MenuHarry on sanboxHarry and PlayerData.cs to implement serialized data

---
+++ Assets/Scripts/Camera_and_Player/PlayerData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// how to save data in Unity using System.Serializable
+// https://dev.to/arkilis/systemserializable-in-unity-25hm
+[System.Serializable]
+public class PlayerData
+{
+    public float x;
+    public float y;
+    public float z;
+
+    public PlayerData(Vector3 position)
+    {
+        x = position.x;
+        y = position.y;
+        z = position.z;
+    }
+}
