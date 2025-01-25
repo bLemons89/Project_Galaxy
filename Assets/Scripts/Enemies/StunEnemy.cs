279ddeec2006986e1ff27669bdd86e692c7039b5
Juan Contreras <JAContreras1@student.fullsail.edu>
2025-01-25 15:43:36 -0500

    2 enemies working now, player prefab modified and working

--- Assets/Scripts/Enemies/StunEnemy.cs
+++ Assets/Scripts/Enemies/StunEnemy.cs
@@ -20,9 +20,9 @@ public class StunEnemy : EnemyBase
     [SerializeField] int roamRadius = 20;
 
     //for interactions with the player
-    GameObject player;
+    //GameObject player;
     GameObject itemModel;       //to attach model to enemy
-    playerScript playerSettings;
+    //playerScript playerSettings;
     enum EnemyState { Roaming, Chasing, Fleeing }            //Behavior changes when taking item
     EnemyState currentState = EnemyState.Roaming;   //Starts by roaming
 
@@ -39,12 +39,6 @@ public class StunEnemy : EnemyBase
         agent.speed *= speed;
         agent.stoppingDistance = distanceFromPlayer;
         roamPosition = agent.destination;
-
-        if (GameManager.instance)
-        {
-            player = GameObject.FindWithTag("Player");
-            playerSettings = player.GetComponent<playerScript>();
-        }
     }
 
     // Update is called once per frame
