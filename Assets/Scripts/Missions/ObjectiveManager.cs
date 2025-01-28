9f46d94f074485d8306cec96d09d4e7c2950f1d5
Juan Contreras <JAContreras1@student.fullsail.edu>
2025-01-28 02:48:34 -0500

    Added objective text/manager and started on objective triggers

---
+++ Assets/Scripts/Missions/ObjectiveManager.cs
@@ -0,0 +1,79 @@
+/*
+    Author: Juan Contreras
+    Edited by:
+    Date Created: 01/28/2025
+    Date Updated: 01/28/2025
+    Description: Class to display the objectives on the UI
+ */
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ObjectiveManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI objectiveText;
+
+    //defining objectives directly in the script as a Dictionary
+    private Dictionary<int, string> objectives = new Dictionary<int, string>
+    {
+        { 0, "Arm yourself" },
+        { 1, "Exit the ship" },
+        { 2, "Defend yourself" },
+        { 4, "Pick up the Energy Cell" },
+        { 5, "Explore the planet for more cells" },
+        { 6, "Search the areas for a cell" },
+        { 7, "Cells collected 2/3" },
+        { 8, "All cells collected. Return to the ship." },
+        { 9, "Survive." }
+    };
+
+    private Queue<int> objectivesQueue = new Queue<int>(); //queue to store the IDs of objectives
+
+    private void Start()
+    {
+        //add the objectives to queue (in order)
+        foreach (var id in objectives.Keys)
+        {
+            objectivesQueue.Enqueue(id);
+        }
+
+        UpdateObjectiveText();
+    }
+
+    //completes the current objective and moves to the next
+    public void CompleteObjective()
+    {
+        if (objectivesQueue.Count > 0)
+        {
+            objectivesQueue.Dequeue(); //remove the current objective
+            UpdateObjectiveText();    //update the UI text
+        }
+        else
+        {
+            //Debug.Log("All objectives completed!");
+        }
+    }
+
+    //updates the UI text with the current objective
+    private void UpdateObjectiveText()
+    {
+        if (objectivesQueue.Count > 0)
+        {
+            int currentObjectiveId = objectivesQueue.Peek();
+            objectiveText.text = objectives[currentObjectiveId];
+        }
+        else
+        {
+            objectiveText.text = "Not bad, insert the cells into the ship and skedaddle!";
+        }
+    }
+
+    //add a new objective directly in code
+    public void AddObjective(string newObjective)
+    {
+        int newId = objectives.Count;           //use the next available ID
+        objectives.Add(newId, newObjective);
+        objectivesQueue.Enqueue(newId);
+        UpdateObjectiveText();
+    }
+}
---
+++ Assets/Scripts/TriggerEvents/ObjectiveTriggers/Objective0.cs
@@ -0,0 +1,40 @@
+/*
+    Author: Juan Contreras
+    Edited by:
+    Date Created: 01/28/2025
+    Date Updated: 01/28/2025
+    Description: Triggers the next mission in the queue if the criteria is met
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objective0 : MonoBehaviour
+{
+    bool playerInRange;
+
+    private void Update()
+    {
+        if(playerInRange && Input.GetKeyDown(KeyCode.Q))
+        {
+            GameManager.instance.GetComponent<ObjectiveManager>().CompleteObjective();
+
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
